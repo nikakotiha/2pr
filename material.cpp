97b0d7a177b5b38e2e065e8df70ea3e0d6a59a92
nikakotiha <veronica.lukashenko@gmail.com>
2015-04-22 10:32:25 +0300

    initial project version

---
+++ material.cpp
@@ -0,0 +1,253 @@
+
+//#include "stdafx.h"
+#include <iostream>
+#include <iomanip>
+#include <locale>
+#include <fstream>
+
+
+using namespace std;
+
+
+
+ struct  source {
+    int id;
+    string Name;
+    double Price;
+	double Weight;
+};
+
+struct List{
+   source data;
+    List *next;
+};
+
+class ListNode
+{
+    private:
+        List *Head;
+		ifstream FileOut;
+		ofstream  FileIn;
+		 char*  NameFile ;
+		 int Count;
+
+    public:
+
+        ListNode():Head(NULL) {}
+
+
+
+
+         void  GetElemByPos( int Pos, const  source value )             //добавление в указанную
+        {
+			 if (Head != NULL)
+			 {
+				 List *tmp = Head;
+
+
+				 int Kol_el = 0;
+				 while (tmp->next != NULL && tmp->data.id != Pos )
+				 {
+					 tmp = tmp->next;
+					 ++Kol_el;
+				 }
+				 if (tmp->data.id == Pos){
+					 tmp->data = value;
+				 }
+			 }
+        }
+
+        void delList(const int value)
+        {
+            List *tmp = Head;
+            List *cur = NULL;
+
+            if(Head != NULL)
+            {
+                if((Head -> data.id) == value)                 //если удаляем 1 элемент
+                {
+                    cur = Head -> next;
+                    delete Head;
+                    Head = cur;
+
+                }
+                else
+                {
+                    while((tmp->next != NULL) && (tmp -> data.id != value))
+                    {
+                        cur = tmp;
+                        tmp = tmp -> next;
+                    }
+
+					if ((tmp->next == NULL) && (tmp->data.id == value))          //если элемент между
+                    {
+															                     //1 и последним
+							delete tmp;
+							cur->next = NULL;
+                    }
+                    else                                    //если элемент последний
+                    {
+						if (tmp->data.id == value){
+							cur->next = tmp->next;
+							delete tmp;
+						}
+                    }
+                }
+            }
+        }
+
+	void clear(){
+		List *temp = Head;
+ 		 List *cur = NULL;
+            if(Head != NULL)
+            {
+                while(temp != NULL)
+                {
+               	    cur=   temp ;
+	                    temp = temp -> next;
+          	    delete cur;
+		 }
+            }
+	}
+        void showList()
+        {
+            List *temp = Head;
+
+            if(Head != NULL)
+            {
+                while(temp != NULL)
+                {
+                    cout << temp -> data.id << ";  "<<temp -> data. Name << ";  "<<temp -> data.Price << ";  "<<temp -> data.Weight<<";"<<"\n";
+
+
+                   temp = temp -> next;
+                }
+            }
+            else
+                cout << "list is empty!"<<"\n";
+        }
+
+
+bool FileWrite()
+		{
+			NameFile= " DB";
+			int sizeName;
+			int sizePrice;
+
+			try
+			{
+
+				List *temp = Head;
+
+				if (Head != NULL)
+				{
+					FileIn.open(NameFile, ios_base::binary | ios::out);
+
+
+
+					FileIn.write((char*)&Count, sizeof(int));
+					while (temp != NULL)
+					{
+						FileIn.write((char*)&temp->data.id, sizeof(temp->data.id));
+						sizeName = temp->data.Name.length();
+
+						FileIn.write((char*)&sizeName, sizeof(sizeName));
+
+						FileIn.write((char*)temp->data.Name.c_str(), sizeof(char) * sizeName);
+
+//					sizePrice = temp->data.Price.length();
+
+						FileIn.write((char*)&temp->data.Price, sizeof(temp->data.Price));
+                        FileIn.write((char*)&temp->data.Weight, sizeof(temp->data.Weight));
+
+						//FileIn.write((char*)temp->data.Price.c_str(), sizeof(char)*sizePrice);
+						temp = temp->next;
+					}
+					FileIn.close();
+				}
+				else
+					throw 1;
+			}
+			catch(...)
+			{
+				return false;
+			}
+			return true;
+		}
+
+		bool FileRead()
+		{
+			NameFile= " DB";
+			int sizeName=0;
+			int sizePrice=0;
+			bool out = true;
+			source data;
+			int i=0;
+			int Col;
+			try
+			{
+
+					FileOut.open(NameFile, ios_base::binary | ios::in);
+
+					if (FileOut.is_open()) {
+
+						FileOut.read((char*)&Col, sizeof(int));
+						while (i < Col)
+						{
+							FileOut.read((char*)&data.id, sizeof(int));
+
+							FileOut.read((char*)&sizeName, sizeof(int));
+							char *tmp = new char[sizeName + 1];
+							tmp[sizeName] = '\0';
+							FileOut.read(tmp, sizeof(char)*sizeName);
+							data.Name = (string)tmp;
+							FileOut.read((char*)&data.Price, sizeof(double));
+							FileOut.read((char*)&data.Weight, sizeof(double));
+							//FileOut.read((char*)&sizePrice, sizeof(int));
+							//char *tmp1 = new char[sizePrice + 1];
+							//tmp1[sizePrice] = '\0';
+							//FileOut.read(tmp1, sizeof(char)*sizePrice);
+//							data.Price = (string)tmp1;
+
+							add(data);
+							i++;
+						}
+						FileOut.close();
+
+					}
+					else{ throw 4; }
+			}
+			catch (...)
+			{
+				out = false;
+			}
+			return out;
+		}
+
+
+
+		 void add(const  source value)                 //добавление в конец
+        {
+            List *Ptr = new (List);
+            Ptr -> data = value;
+
+            if(Head == NULL)
+            {
+                Head = Ptr;
+                Ptr -> next = NULL;
+				Count=1;
+            }
+            else
+            {
+                List *temp = Head;
+
+                while(temp -> next != NULL)
+                    temp = temp -> next;
+
+                temp -> next = Ptr;
+                Ptr -> next = NULL;
+
+  	Count++;
+            }
+        }
+		 };
---
+++ Практ2.cpp
@@ -0,0 +1,201 @@
+﻿// Практ2.cpp : Defines the entry point for the console application.
+//
+
+//#include "stdafx.h"
+
+
+#include "material.cpp"
+#include <stdio.h>
+#include <iostream>
+
+ #include <stdlib.h>
+     //#include <termios.h>
+     #include <string.h>
+
+using namespace std;
+
+bool isNumber(string str, int j)
+{
+    unsigned length = str.length();
+    if (length>j)
+    {
+        return false;
+    }
+    for (unsigned i=0; i<length; ++i)
+   if (str[i] <'9' || str[i] >'0' || str[i]=='.'  ) return true;
+   return false;
+  //if (str[i] >'9' || str[i] <'0' || str[i]!='.'  ) return false;
+    //return true;
+
+}
+
+
+source dataSet(){
+	source tmp;
+	double temp;
+	bool prov=false;
+	string num;
+	//cout << "Enter id library" << "\n";
+	//cin >> tmp.id;
+	//while ((tmp.id < 0)){
+	//cerr << "Error while reading the integer value > 0" << endl;
+	 do {
+	 do
+        {
+        cout << "Enter id" << "\n";
+             cin >> num;
+            prov=isNumber(num, 6);
+
+        } while ((prov==false));
+        temp=atof(num.c_str());
+ if (temp==0 or temp<0)
+        cout << "retry" << "\n";
+        tmp.id =temp;
+        } while(temp==0 or temp<0);
+	//cin.clear();
+	//cin.sync();
+	//cin >> tmp.id;
+	//}
+	cout << "Enter  Name   " << "\n";
+	cin >> tmp.Name;
+ do {
+	 do
+        {
+        cout << "Enter price " << "\n";
+             cin >> num;
+            prov=isNumber(num, 6);
+
+        } while ((prov==false));
+        temp=atof(num.c_str());
+ if (temp==0)
+        cout << "retry" << "\n";
+        tmp.Price=temp;
+        } while(temp==0);
+	 do{
+	 do
+        {
+       	cout << "Enter Weight " << "\n";
+            cin >> num;
+
+            prov=isNumber(num, 6);
+
+        } while ((prov==false));
+
+       temp=atof(num.c_str());
+        if (temp==0)
+        cout << "retry" << "\n";
+        tmp.Weight=temp;
+        }
+        while(temp==0);
+
+
+	return tmp;
+}
+
+int  main()
+{
+	char i = '0';
+	int id;
+	char tmp;
+
+	ListNode DB;
+
+	do {
+		switch (i)
+		{
+		case '0':
+			/* Main menu*/
+
+	//		cin.clear();
+		//	cin.sync();
+			cout << "Add (1)"<<"\n" ;
+			cout << "Delete(2)" << "\n";
+ 			cout << "Change(3)" << "\n";
+ 			cout << "Show list(4)" << "\n";
+			cout << "File write(5)" << "\n";
+			cout << "File read(6)" << "\n";
+			cout << "Exit(7)" << "\n";
+			//cin >> i;
+
+			break;
+
+		case '1':
+			/* Добавление */
+
+			cout << "Enter new  data" << "\n";
+			DB.add(dataSet());
+			i = '0';
+			break;
+
+		case '2':
+			/*Удаление */
+
+			cout << "Enter record id, you want to delete " << "\n";
+			while (!(cin >> id) || (id < 0)){
+				cerr << "Error while reading the integer value > 0" << endl;
+				cin.clear();
+				cin.sync();
+			}
+			DB.delList(id);
+			i = '0';
+			break;
+
+		case '3':
+			/*Изменение */
+
+			cout << "Enter record id, you want to change " << "\n";
+			while (!(cin >> id) || (id < 0)){
+				cerr << "Error while reading the integer value > 0" << endl;
+				cin.clear();
+				cin.sync();
+			}
+			DB.GetElemByPos(id, dataSet());
+			i = '0';
+			break;
+
+		case '4':
+			/* Просмотр */
+
+			DB.showList();
+
+			cin>>tmp;
+			i = '0';
+			break;
+
+	 case '5':
+
+
+	 if (!(DB.FileWrite()))
+	 {
+	 cerr << "File error!!" << endl;
+	 }
+	 i = '0';
+	 break;
+
+ case '6':
+
+
+	 if (!(DB.FileRead()))
+	 {
+	 cerr << "File error!!" << endl;
+
+	 }
+	 i = '0';
+	 break;
+
+ case '7':
+
+	 DB.clear();
+	 cin>>tmp;
+
+	break;
+//default:
+//cout << "You entered the wrong character! Try again"<<"\n";
+//break;
+}
+i=getchar();
+//i=getchar();
+} while (i != '7');
+	DB.clear();
+	return 0;
+};
